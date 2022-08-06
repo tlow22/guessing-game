e00a1cd947600f4e2922ee2b74d7fb7c9a853cd1
tlow22 <thaddeuslow@outlook.com>
2022-08-06 15:53:42 -0600

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ const MAX_NUMBER: u32 = 100;
 const MIN_NUMBER: u32 = 1;
 const MAX_NUMBER_OF_GUESS: u32 = 3;
 
+
 fn main() {
     // generate a random number (integer) in range [1-100] 
     let mut rng = rand::thread_rng();
     let winning_number: u32 = rng.gen_range(MIN_NUMBER..=MAX_NUMBER);
     println!("Winning number = {}", winning_number);
     
-    // prompt user for lottery guess   
+    // prompt user for lottery guess (3 tries max)  
     let mut number_of_tries = 0;
     let mut is_win: bool = false;
     
@@ -27,31 +28,35 @@ fn main() {
             .read_line(&mut user_input)
             .expect("Failed to read line");
     
-        let user_input: u32 = user_input.trim().parse().expect("Please enter a number");
-
-        // check user input validity 
-        if user_input < MIN_NUMBER || user_input > MAX_NUMBER {
-            println!("{} is an invalid number", user_input );
-            continue;
-
-        } else {
-            // check against winning number 
-            println!("You guessed: {user_input}");
-
-            match user_input.cmp(&winning_number) {
-                Ordering::Less => println!("Too small!"),
-                Ordering::Equal => {
-                    is_win = true; 
-                    break;
+        let user_input: u32 = match user_input.trim().parse() {
+            Ok(num) => {
+                if in_valid_guess_range(num) {
+                    num
+                } else {
+                    println!("Provided guess '{}' is out of valid range", user_input);
+                    continue
                 }
-                Ordering::Greater => println!("Too big!"),
+            }, 
+            Err(_) => continue
+        };
+
+        // check against winning number 
+        println!("You guessed: {user_input}");
+
+        match user_input.cmp(&winning_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                is_win = true; 
+                break;
             }
-            
-            number_of_tries = number_of_tries + 1;
+            Ordering::Greater => println!("Too big!"),
         }
+        
+        number_of_tries = number_of_tries + 1;
+        
     }
 
-    // 
+    // Inform user of win/loss 
     match is_win {
         true => {
             println!("Winning number = {}", winning_number);
@@ -59,12 +64,13 @@ fn main() {
         }
         false => println!("You get nothing! You lose! Good day sir!")
     }
-    
-    
-    
+}
 
-    
 
-    
-    
+fn in_valid_guess_range(x: u32) -> bool {
+    if x >= MIN_NUMBER && x <= MAX_NUMBER {
+        return true
+    } else {
+        return false
+    }
 }

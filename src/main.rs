0533e0c93dcfae3d4f737cf1708b25470ca99844
tlow22 <thaddeuslow@outlook.com>
2022-08-06 15:00:06 -0600

    add random lottery selection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::io;
-
+use rand::Rng;
 fn main() {
+    // generate a random number (integer) in range [1-100] 
+    let mut rng = rand::thread_rng();
+    let winning_number: u32 = rng.gen_range(1..=100);
+    println!("Winning number = {}", winning_number);
+
+    // prompt user for lottery guess   
     println!("Guess a number between 1-100");
     let mut user_input: String = String::new();
     
@@ -9,4 +15,6 @@ fn main() {
         .expect("Failed to read line");
     
     println!("You guessed: {user_input}")
+
+    
 }
